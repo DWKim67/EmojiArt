9ee72463a3c489d123a895451e7b64628b99aa40
DWKim67 <dkim67@my.yorku.ca>
2024-01-13 14:21:49 -0500

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Daniel Kim on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
