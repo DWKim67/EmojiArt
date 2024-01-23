c73ad16d7d0535eac27b3f583cd5b7f3216fc719
DWKim67 <dkim67@my.yorku.ca>
2024-01-23 10:21:59 -0500

    Up to Lect 14 notes

---
+++ Emoji Art/PaletteList.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteList.swift
+//  Emoji Art
+//
+//  Created by Daniel Kim on 2024-01-21.
+//
+
+import SwiftUI
+
+struct PaletteList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteList()
+}
